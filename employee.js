3edc9a730ee32d7f68172ba7c242c857778e8755
Marc Fernandez <mfernandez225@gmail.com>
2020-01-31 22:52:11 -0800

    Tests passing for Roles

--- employee.js
+++ employee.js
@@ -1,13 +1,21 @@
 class Employee {
-  constructor(data) {
-    if (!data) throw "No data"
-    if (!data.name) throw "Name is required"
-    if (!data.id) throw "ID is required"
-    if (!data.title) throw "Title is required"
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
 
-    this.name = data.name;
-    this.id = data.id;
-    this.title = data.title;
+  getName() {
+    return this.name
+  }
+  getId() {
+    return this.id
+  }
+  getEmail() {
+    return this.email
+  }
+  getRole() {
+    return "Employee"
   }
 }
 
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,5 +1,4 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const Manager = require("../manager");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
@@ -7,13 +6,13 @@ test("Can set office number via constructor argument", () => {
   expect(e.officeNumber).toBe(testValue);
 });
 
-test("getRole() should return \"Manager\"", () => {
+test(`getRole() should return "Manager"`, () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
   expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get office number via getOffice()", () => {
+test("Can get office number via getOfficeNumber()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
